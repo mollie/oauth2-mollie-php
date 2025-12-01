892e66de64e55035ee87c6f026d9197a22274cd0
Krishan König <Naoray@users.noreply.github.com>
2025-12-01 09:41:53 +0100

    Update Mollie.php

--- src/Provider/Mollie.php
+++ src/Provider/Mollie.php
@@ -17,7 +17,7 @@ class Mollie extends AbstractProvider
     /**
      * Version of this client.
      */
-    const CLIENT_VERSION = "2.8.4";
+    const CLIENT_VERSION = "2.9.0";
 
     /**
      * The base url to the Mollie API.
